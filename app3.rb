32d9d428f1eee11bb15904b97a911a185d416624
silent <silent161192@gmail.com>
2017-08-21 18:38:00 +0300

    x ==f

--- app3.rb
+++ app3.rb
@@ -1,14 +1,18 @@
 print "Сколько раз будем играть: "
 n = gets.to_i
 
+print "введите вашел любимое число: "
+f = gets.to_i
+
 1.upto(n) do |nn|
+
 
 	puts "We play #{nn} times"
 	sleep 2
 
 	x = rand(1..10)
 
-	if x == 7
+	if x == f
 		sleep 3
 		puts "Winner"
 		exit
