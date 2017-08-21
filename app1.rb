07b393b44c1a85f46c179f62999a9f3ea50057db
silent <silent161192@gmail.com>
2017-08-21 17:34:14 +0300

    else

--- app1.rb
+++ app1.rb
@@ -3,6 +3,14 @@ answer = gets.strip.capitalize
 
 if answer == "Да"
 	puts "Очень хорошо"
+		if answer == ''
+			print "Введите Да или Нет"
+		
+	else answer == "Нет"
+		puts "Очень плохо"
+	end
 end
+
+puts "This is good"
 
 
