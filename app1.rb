6af115059f1254d013cc3ca1491c1ac1f9c68dc4
silent <silent161192@gmail.com>
2017-08-21 17:51:50 +0300

    strip

--- app1.rb
+++ app1.rb
@@ -1,14 +1,12 @@
 print "Вы довольны своей зарплатой?: (Да/Нет)"
-answer = gets.strip.capitalize
+answer = gets.strip.capitalize!
 
 if answer == "Да"
 	puts "Очень хорошо"
-		if answer == ''
-			print "Введите Да или Нет"
 		
-	else answer == "Нет"
+else answer == "Нет"
 		puts "Очень плохо"
-	end
+	
 end
 
 puts "This is good"
